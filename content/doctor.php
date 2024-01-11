7fb5b6959d3be66699994efc5bc87c8f7d673461
dhunganaPradeep <pradeep.dhungana23@gmail.com>
2024-01-11 14:44:04 +0545

    added navbar to seperate files

--- content/doctor.php
+++ content/doctor.php
@@ -45,73 +45,7 @@ if (!isset($_SESSION['uid'])) {
     <!-- 
     - #HEADER
   -->
-
-    <header class="header" data-header>
-        <div class="container">
-            <div class="overlay" data-overlay></div>
-
-            <a href="../index.php" class="logo">
-                <img src="../assets/images/logo.svg" width="250px" alt="PulsePath logo" />
-            </a>
-
-            <button class="nav-open-btn" data-nav-open-btn aria-label="Open Menu">
-                <ion-icon name="menu-outline"></ion-icon>
-            </button>
-
-            <nav class="navbar" data-navbar>
-                <button class="nav-close-btn" data-nav-close-btn aria-label="Close Menu">
-                    <ion-icon name="close-outline"></ion-icon>
-                </button>
-
-                <a href="../index.php" class="logo">
-                    <img src="../assets/images/logo.svg" alt="PulsePath logo" />
-                </a>
-
-                <ul class="navbar-list">
-                    <li class="navbar-item">
-                        <a href="doctor.php" class="navbar-link"><strong>Doctors</strong></a>
-                    </li>
-
-                    <li class="navbar-item">
-                        <a href="traffic.php" class="navbar-link"><strong>Traffic Control</strong></a>
-                    </li>
-
-                    <li class="navbar-item">
-                        <a href="hospital.php" class="navbar-link"><strong>Hospitals</strong></a>
-                    </li>
-                    <li class="navbar-item">
-                        <a href="mapnav.php" class="navbar-link"><strong>Map</strong></a>
-                    </li>
-                </ul>
-
-                <!-- Dropdown Menu -->
-                <ul class="nav-action-list">
-                    <li id="profile-dropdown">
-                        <div class="nav-action-btn">
-                            <ion-icon name="person-outline" aria-hidden="true"></ion-icon>
-                            <span class="nav-action-text">Login / Register</span>
-                        </div>
-                        <div class="dropdown-content">
-                            <div class="user-info">
-                                <img src="../assets/images/doc1.jpeg" alt="User Avatar" class="user-avatar" />
-                                <div class="user-details">
-                                    <p class="user-name"><?php echo $name; ?></p>
-                                </div>
-                            </div>
-                            <hr />
-                            <a href="#">Information Page</a>
-                            <hr />
-                            <a href="#">Vehicle Information Page</a>
-                            <hr />
-                            <a href="#">Trips Page</a>
-                            <hr />
-                            <a href="user/logout.php">Logout</a>
-                        </div>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+  <?php include 'navbar/navbar.php'; ?>
 
     <!-- Doctor Section -->
     <section class="doctors-section">
@@ -174,12 +108,6 @@ if (!isset($_SESSION['uid'])) {
     - custom js link
   -->
     <script src="../assets/js/script.js"></script>
-
-    <!-- 
-  - ionicon link
--->
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
